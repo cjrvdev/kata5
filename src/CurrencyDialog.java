0b4af6fd86edcea0ca752ca5b7ba8eff060fcb57
Tronx <Tronx@Tronx-PC>
2015-01-14 12:23:59 +0000

    entrega

---
+++ src/CurrencyDialog.java
@@ -0,0 +1,15 @@
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+class CurrencyDialog extends JPanel {
+
+    public CurrencyDialog() {
+        super();
+        add(createComboBox());
+    }
+
+    private JComboBox createComboBox() {
+        return new JComboBox(new String[]{"EUR","USD","GBP"});
+    }
+    
+}
---
+++ src/Kata5.java
@@ -0,0 +1,7 @@
+public class Kata5 {
+
+    public static void main(String[] args) {
+        new MoneyCalculatorFrame();
+    }
+    
+}
